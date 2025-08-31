c727c98ac79ce6ad9c9b987dffdc36b7a1aa5bf1
Augi01 <augifrancodeguzman01@gmail.com>
2025-08-31 14:06:12 +0800

    Create Probelem_2.cs
    created by augi problem 2

---
+++ Probelem_2.cs
@@ -0,0 +1,15 @@
+using System;
+
+class OddNumbers
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 50; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
